eac89a9eac52fd0fb0b24b7842dbe72e7a4973ac
Dima Pristupa <dima.pristupa@pm.me>
2023-03-23 18:19:13 +0200

    🚧 NextUI: setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
+import { createTheme, NextUIProvider } from '@nextui-org/react'
 import type { AppProps } from 'next/app'
 
+const theme = createTheme({
+    type: 'dark',
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <NextUIProvider theme={theme}>
+            <Component {...pageProps} />
+        </NextUIProvider>
+    )
 }
